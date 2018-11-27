cb1deccda652ab2ced6c3054923fc05069530c40
DC23 <jugglindan@gmail.com>
2018-11-28 10:57:56 +1100

    increasing conky load delay due to rendering issues on laptop

--- .Conky2cpu/startconky.sh
+++ .Conky2cpu/startconky.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#sleep 10 #time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
+sleep 20 #time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
 conky -c ~/.Conky2cpu/rings & # the main conky with rings
-sleep 8 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
+sleep 10 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
 conky -c ~/.Conky2cpu/cpu &
-sleep 1
+sleep 5
 conky -c ~/.Conky2cpu/mem &
 #conky -c ~/.Conky2cpu/notes &
