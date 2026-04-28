60d2da2640f9a39af503f15ee07047fd821e3126
DC23 <DC23@users.noreply.github.com>
2026-04-28 10:33:15 +1000

    added loop

--- run-md-2-bb.sh
+++ run-md-2-bb.sh
@@ -1,2 +1,12 @@
-cd "/mnt/c/Users/juggl/OneDrive/Documents/Obsidian/RPG/Campaigns/Ravens Call"
-md_to_bbcode.py "Scenes/Scene 30.md" --offset -2 --output "published/output30.md"
+#!/bin/bash
+
+RAVENS_CALL="/mnt/c/Users/juggl/OneDrive/Documents/Obsidian/RPG/Campaigns/Ravens Call"
+START=33
+END=34
+OFFSET=-3
+
+cd "$RAVENS_CALL"
+
+for SCENE in $(seq $START $END); do
+    md_to_bbcode.py "Scenes/Scene $SCENE.md" --offset $OFFSET --output "published/scene_$SCENE.txt"
+done
